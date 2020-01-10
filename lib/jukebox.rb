5663d45ffa38725dffb6223ce1de0d2584b223fe
Lee Bardon <leerbardon@gmail.com>
2020-01-10 22:38:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,7 +16,7 @@ end
 
 def play(songs)
     puts "Please enter a song name or number:"
-    user = gets.chomp
+    input = gets.chomp
     hash = {}
     songs.each.with_index(1) do |song, num|
       hash.merge!("#{num}" => "#{song}")
